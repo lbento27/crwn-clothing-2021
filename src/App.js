07f38e66a0d978f9573bf2767e79238a042338a0
Bento <lbento525@gmail.com>
2021-08-25 00:55:39 +0100

    added storing user data in firebase and in our app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -22,10 +22,26 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      //createUserProfileDocument(user);
+      //this.setState({ currentUser: user });
       //console.log(user);
+      //check if user sign in
+      if (userAuth) {
+        //if exists, userAuth is null when signing out
+        const userRef = await createUserProfileDocument(userAuth);
+
+        //get data from the created user or from the already stored user with snapshot and save to the sate object
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            //snapshot is a method in fireStore that gives us some property´s to get that reference "store" data we use .data() o that snapshot
+            id: snapshot.id,
+            ...snapshot.data(),
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth }); //same as currentUser = null, if logs out
+      }
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,34 @@ const config = {
   measurementId: "G-J8RP5ZMFLB",
 };
 
+//store the user in our database created by firebase auth
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return; //if theres no user
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`); // get that user reference at that location, "users/" that collection, that uid is a property that the authentication with google , firebase automatic creates for every user
+  const snapShot = await userRef.get(); //get user .get() gives us an object with a property exists
+
+  //check if we already store that user object
+  if (!snapShot.exists) {
+    //if do not exist create that user
+    const { displayName, email } = userAuth; //get displayName and email from userAuth
+    const createdAt = new Date();
+    //save that user so we use try catch
+    try {
+      //.set() = create method
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData, //spread any additional data
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef; //return if we want to use it later
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
